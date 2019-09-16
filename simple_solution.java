9ee95ecc2360e008e3a4f5aebf1d3f0dce477ecb
IgniteCoder <55241089+IgniteCoder@users.noreply.github.com>
2019-09-16 22:03:07 +0530

    Add files via upload
    Simplest Solution With O(n^2) Time Complexity.

---
+++ simple_solution.java
@@ -0,0 +1,27 @@
+class simple_solution{
+
+   static void getOccuringChar(String S)
+     {
+   		int n =S.length();
+		char c[]= new char[n];
+		c= S.toCharArray();
+		int count;
+		for(int i=0;i<n;i++)
+		{
+			count=0;
+		    for(int j=0;j<n;j++)
+			{	
+				if(c[i]==c[j])
+				count++;
+			}
+			System.out.print(c[i] +" : " +count + "  ");
+		}
+	}
+	public static void main(String[] args) 
+    { 
+        Scanner sc = new Scanner(System.in); 
+        String str = sc.nextLine(); 
+        getOccuringChar(str); 
+    } 
+
+}
