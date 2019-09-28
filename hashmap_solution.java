cbb153a58ec16fa1b25a8984b5f376c85a295f03
IgniteCoder <55241089+IgniteCoder@users.noreply.github.com>
2019-09-28 14:30:52 +0530

    Add files via upload
    This Solution Contains Solution Using HashMap With Time Complexity O(n) and Space Complexity (n);

---
+++ hashmap_solution.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class hashmap_solution{
+
+   static void getOccuringChar(String S)
+    {
+    	HashMap <Character,Integer> map = new HashMap<>();
+    	int n= S.length(),count=0;
+    	for(int i=0;i<n;i++)
+    	{
+    		//check if hashmap already has our searching character
+    		if(map.containsKey(S.charAt(i)))
+    		{
+    			count=map.get(S.charAt(i)); //gets already existing value
+    			map.replace(S.charAt(i),count+1); //puts new value which is old value + 1
+    		}
+    		//if its new char we add to hashmap with value 1
+    		else
+    			map.put(S.charAt(i),1);
+    	}
+    	//lets print the map with required output format
+   		for (String i : map.keySet())
+   		 {
+   		   System.out.print( i + " : " + map.get(i)+"  ");
+	     }
+	public static void main(String[] args) 
+    { 
+        Scanner sc = new Scanner(System.in); 
+        String str = sc.nextLine(); 
+        getOccuringChar(str); 
+    } 
+
+}
